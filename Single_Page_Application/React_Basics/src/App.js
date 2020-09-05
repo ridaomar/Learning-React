61998f6bc11cdcef6d2f27ce41c893fa64368153
ridhaomar <labbar2010@gmail.com>
2020-09-05 13:18:35 +0200

    use Internal Functional Component

--- Single_Page_Application/React_Basics/src/App.js
+++ Single_Page_Application/React_Basics/src/App.js
@@ -5,8 +5,20 @@ function App() {
   return (
     <div className="App">
         The App is working!
+        <br/>
+
+        <h3 style={{marginBottom: 0}}>
+          Types of Component
+        </h3>
+
+        {/* Internal Functional Component */}
+        <InternalFunctionalComponent  />
     </div>
   );
+}
+
+function InternalFunctionalComponent() {
+  return <div>Internal functional component is working!</div>
 }
 
 export default App;
